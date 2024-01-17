414057468b5c329bb178e22bb60764850846fd3b
adityaweb3 <ap7596112@gmail.com>
2024-01-17 21:26:45 +0530

    triangle

---
+++ 2D DP/triangle.cpp
@@ -0,0 +1,26 @@
+// You are given a triangular array/list 'TRIANGLE'. Your task is to return the minimum path
+//  sum to reach from the top to the bottom row.
+// The triangle array will have N rows and the i-th row, where 0 <= i < N will have i + 1 elements.
+// You can move only to the adjacent number of row below each step. For example, 
+// if you are at index j in row i, then you can move to i or i + 1 index in row j + 1 in each step.
+
+#include <bits/stdc++.h> 
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	// Write your code here.
+	int dp[n][n] ; 
+
+	for(int i = 0 ; i<n; i++){
+		dp[n-1][i]=triangle[n-1][i] ;
+	}
+
+	for(int i = n-2; i>=0 ; i--){
+		for(int j = i ; j>=0 ; j--){
+			int d = triangle[i][j] + dp[i+1][j] ; 
+			int  dg = triangle[i][j] + dp[i+1][j+1] ; 
+
+			dp[i][j] = min(d,dg) ; 
+		}
+	}
+
+	return dp[0][0] ;
+}
